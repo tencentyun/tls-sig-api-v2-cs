96fbc867cbd50d87cfe23be8e8158077a5531c56
weijunyi <weijunyi@tencent.com>
2019-06-04 15:54:16 +0800

    增加代码

--- tls-sig-api-v2-cs/TLSSigAPI.cs
+++ tls-sig-api-v2-cs/TLSSigAPI.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 
-namespace tls_sig_api_v2_cs
+
+namespace tecentyun
 {
-    public class Class1
+    public class TLSSigAPI
     {
+        private readonly int sdkappid;
+        private readonly string key;
+
+        public TLSSigAPI(int sdkappid, string key)
+        {
+            this.sdkappid = sdkappid;
+            this.key = key;
+        }
+
+        public string GenSig(string identifier, int expireTime)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1); // unix 时间戳
+            Int64 currTime = (Int64)(DateTime.UtcNow - epoch).TotalMilliseconds / 1000;
+
+            string rawContentToBeSigned = "TLS.identifier:" + identifier + "\n"
+                 + "TLS.sdkappid:" + sdkappid + "\n"
+                 + "TLS.time:" + currTime + "\n"
+                 + "TLS.expire:" + expireTime + "\n";
+            using (HMACSHA256 hmac = new HMACSHA256())
+            {
+            }
+            return "";
+        }
     }
 }
