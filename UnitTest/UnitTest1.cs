17d9c18a0b6dea5bbfcd4915d6a6fb409696c6b9
yutingzeng <yutingzeng@tencent.com>
2020-09-08 17:07:22 +0800

    增加使用字符串房间号生成privateMapKey

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -30,5 +30,12 @@ namespace UnitTest
             string sig = api.genPrivateMapKey("xiaojun", 86400 * 180, 10000, 255);
             System.Console.WriteLine(sig);
         }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            TLSSigAPIv2 api = new TLSSigAPIv2(1400000000, "5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e");
+            string sig = api.genPrivateMapKeyWithStringRoomID("xiaojun", 86400 * 180, "aaa", 255);
+            System.Console.WriteLine(sig);
+        }
     }
 }
